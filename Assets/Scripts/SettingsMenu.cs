fe768661a3dcc285cfd343dbf7b40b4cce9d3136
Anamaría Soriano <anasperez10@gmail.com>
2024-11-26 16:38:25 -0500

    Barra de Vida

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -6,6 +6,7 @@ public class SettingsMenu : MonoBehaviour
     public AudioMixer audioMixer;
     public void SetVolume (float volume)
     {
+        Time.timeScale = 1;
         audioMixer.SetFloat("volume", volume);
     }
 }
