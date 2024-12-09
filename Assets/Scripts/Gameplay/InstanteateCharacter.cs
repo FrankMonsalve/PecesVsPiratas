250d62c0c149d468ad7d11811d01778c4af26ff2
Peterquitian <peterquitian@gmail.com>
2024-12-09 18:10:31 -0500

    [Add] Shader OutLine

--- Assets/Scripts/Gameplay/InstanteateCharacter.cs
+++ Assets/Scripts/Gameplay/InstanteateCharacter.cs
@@ -8,6 +8,9 @@ public class InstanteateCharacter : MonoBehaviour
     [SerializeField] private CharacterList characterList; // Lista de prefabs de personajes
     [SerializeField] private Tilemap tilemap; // Tilemap para calcular posiciones en el mundo
 
+    [Header("Player Colors")]
+    [SerializeField] private Color _colorPlayerOne;
+    [SerializeField] private Color _colorPlayerTwo;
     private List<Character> player1 = new List<Character>();
     private List<Character> player2 = new List<Character>();
 
@@ -50,13 +53,15 @@ public class InstanteateCharacter : MonoBehaviour
             // Asigna el personaje al jugador correspondiente
             if (item.PlayerId == 1)
             {
-                character.GetComponent<SpriteRenderer>().color= new Color(0.5f, 0.8f, 1f, 1f);
+                //character.GetComponent<SpriteRenderer>().color= new Color(0.5f, 0.8f, 1f, 1f);
+                character.GetComponent<SpriteRenderer>().material.SetColor("_PlayerColor", _colorPlayerOne);
                 character.GetComponent<SpriteRenderer>().flipX = false;
                 player1.Add(character);
             }
             else if (item.PlayerId == 2)
             {
-                character.GetComponent<SpriteRenderer>().color= new Color(1f, 0.4f, 0.4f, 1f);
+                //character.GetComponent<SpriteRenderer>().color= new Color(1f, 0.4f, 0.4f, 1f);
+                character.GetComponent<SpriteRenderer>().material.SetColor("_PlayerColor", _colorPlayerTwo);
                 character.GetComponent<SpriteRenderer>().flipX = true;
                 player2.Add(character);
             }
